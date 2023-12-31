a2d53546482a49dc0eb5baa1a68386335a98f9cb
mangesh vispute <mangesh2003vispute@gmail.com>
2023-12-31 22:29:25 +0530

    added cote

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 function About() {
-  return <div>Hellow i am mangesh</div>;
+  return (
+    <div className="container mt-5">
+      <blockquote className="blockquote text-center">
+        <p className="mb-0">
+          <strong> Every thing is possible,</strong>
+          <br />
+          Every thing is in the scope of the process,
+          <br />
+          Your work is to{" "}
+          <strong>get the process from the right mentor and work hard</strong>
+          <br />
+          <strong>to complete the process, and complete it.</strong>
+          <br />
+        </p>
+        <footer className="blockquote-footer" style={{ textAlign: "right" }}>
+          Mangesh vispute
+        </footer>
+      </blockquote>
+    </div>
+  );
 }
 
 export default About;
